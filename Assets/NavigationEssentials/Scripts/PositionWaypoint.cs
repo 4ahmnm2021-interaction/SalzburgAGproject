5ddd9f2548aef3169486c662b3139b8c2ee48108
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-05-25 11:19:58 +0200

    Import Navigation Package

---
+++ Assets/NavigationEssentials/Scripts/PositionWaypoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionWaypoint : MonoBehaviour
+{
+    public int id;
+    private void Update()
+    {
+        if (id < drawNavPath.instance.path.corners.Length)
+        gameObject.transform.position = drawNavPath.instance.path.corners[id];
+    }
+    
+
+}
