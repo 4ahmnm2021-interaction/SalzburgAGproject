01ae261f586f24b9debf678e8d558c270e6ec6bc
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2021-06-08 12:40:42 +0200

    Import Assets from Asset Project

---
+++ Assets/Scripts/VentilSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VentilSound : MonoBehaviour
+{
+
+    public AudioClip Ventil;
+    public AudioSource audios;
+
+    public void VentilSounds()
+    {
+        audios.PlayOneShot(Ventil);
+    }
+}
